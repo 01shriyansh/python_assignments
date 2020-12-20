0bf9010d3789b31b8addfcd19f26b24554e80c14
01shriyansh <41980072+01shriyansh@users.noreply.github.com>
2020-12-20 23:03:10 +0530

    Add files via upload

---
+++ Python Assignment 2/a2q1.py
@@ -0,0 +1,9 @@
+# create the pattern.
+
+
+for j in range(1,6):
+	print("* "*j)
+
+for j in range(1,5):
+	print("* "*(5-j))
+
---
+++ Python Assignment 2/a2q2.py
@@ -0,0 +1,7 @@
+# Write a Python program to reverse a word after accepting the input from the user.
+
+var=input("Enter the word :  ")  #input from user
+
+var=var[::-1]           # to reverse the word
+
+print(var)
